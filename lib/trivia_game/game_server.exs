d0494de169724d0614e9136fd90f6748d0bbe538
darren277 <darren277@yahoo.com>
2024-12-31 15:13:33 +0000

    Some changes to game_server.

--- lib/trivia_game/game_server.exs
+++ lib/trivia_game/game_server.exs
@@ -8,21 +8,43 @@ defmodule TriviaGame.GameServer do
   end
 
   def init(room_id) do
-    {:ok, %{}}
+    # We store room_id in state so we can broadcast to the correct room
+    {:ok, %{room_id: room_id, current_question: nil}}
   end
 
-  def handle_cast({:start_question, question}, state) do
-    # Handle starting a new question
-    {:noreply, Map.put(state, :current_question, question)}
+   # Public API for starting a question
+   def start_question(room_id, question, choices) do
+    GenServer.cast({:global, :"game_#{room_id}"}, {:start_question, question, choices})
   end
 
-  def handle_cast({:end_question, correct_answer}, state) do
-    # Handle ending a question and calculating results
-    broadcast_results(state[:room_id], correct_answer)
-    {:noreply, Map.put(state, :current_question, nil)}
+  ## Callbacks
+
+  # :start_question will broadcast a "new_question" event to all clients in "room:#{room_id}"
+  def handle_cast({:start_question, question, choices}, state) do
+    TriviaGameWeb.Endpoint.broadcast!(
+      "room:#{state.room_id}",
+      "new_question",
+      %{question: question, choices: choices}
+    )
+
+    # Track the current question in memory
+    new_state = state
+                |> Map.put(:current_question, %{question: question, choices: choices})
+    {:noreply, new_state}
   end
 
-  defp broadcast_results(room_id, correct_answer) do
-    TriviaGameWeb.Endpoint.broadcast!("room:#{room_id}", "vote:results", %{correct_answer: correct_answer})
-  end
+  # def handle_cast({:start_question, question}, state) do
+  #   # Handle starting a new question
+  #   {:noreply, Map.put(state, :current_question, question)}
+  # end
+
+  # def handle_cast({:end_question, correct_answer}, state) do
+  #   # Handle ending a question and calculating results
+  #   broadcast_results(state[:room_id], correct_answer)
+  #   {:noreply, Map.put(state, :current_question, nil)}
+  # end
+
+  # defp broadcast_results(room_id, correct_answer) do
+  #   TriviaGameWeb.Endpoint.broadcast!("room:#{room_id}", "vote:results", %{correct_answer: correct_answer})
+  # end
 end
